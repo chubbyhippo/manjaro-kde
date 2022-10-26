f8fd2f34ab8440a0dea281d81b18ff7fffaf9d2f
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2022-10-26 15:22:04 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,16 @@
 
+#!/bin/sh
+
+pamac update --no-confirm
+pamac install vim tmux nvm ttf-jetbrains-mono docker docker-compose noto-fonts-emoji --no-confirm
+pamac build google-chrome jetbrains-toolbox --no-confirm
+echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
+source /usr/share/nvm/init-nvm.sh
+
+sudo usermod -aG docker $USER
+sudo systemctl enable docker.service
+
+curl https://raw.githubusercontent.com/chubbyhippo/vimrc/master/.vimrc -o ~/.vimrc
+curl https://raw.githubusercontent.com/chubbyhippo/ideavimrc/main/.ideavimrc -o ~/.ideavimrc
+
+reboot
